956e925838f247b3243da929733b474cdd9af4cc
Elia <mkumboelia@gmail.com>
2023-06-19 02:13:58 +0300

    updated some server code

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 import cv2
+import logging
 from flask import Flask
-from sms import SendSMS
-from flask_apscheduler import APScheduler
+# from sms import SendSMS
+# from flask_apscheduler import APScheduler
 
 from preprocessor import PlateDetector
 
-# set configuration values
+from apscheduler.schedulers.background import BackgroundScheduler
 
-
-class Config:
-    SCHEDULER_API_ENABLED = True
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 
 app = Flask(__name__)
-app.config.from_object(Config())
+# app.config.from_object(Config())
 
 # initialize scheduler
-scheduler = APScheduler()
-scheduler.init_app(app)
+scheduler = BackgroundScheduler()
+# scheduler.init_app(app)
 
 # Initialize plate detector
 plateDetect = PlateDetector(0.25)
@@ -26,10 +28,36 @@ plateDetect = PlateDetector(0.25)
 # OpenCv video
 capture = cv2.VideoCapture('./test_videos/vid3.mp4')
 
-# plateDetect.process_image("./test_videos/meme_pic.jpg")
+
+def detect_and_recognize_plate():
+    # Add your plate detection and recognition code here
+    # This function will be called every 5 seconds by the scheduler
+    ret, frame = capture.read()
+    # Process the frame to detect and recognize the plate
+    # Print the plate number
+    print(plateDetect.process_image(frame))
+
+# @scheduler.task('interval', id='detect_plate', seconds=60, misfire_grace_time=900)
+# def detect_plate():
+#     success, frame = capture.read()
+#     fps = capture.get(cv2.CAP_PROP_FPS)
+#     logging.info(f"Taking a picture of video with, {fps} FPS")
+
+#     if not success:
+#         # Handle video capture error
+#         logging.info("Error capturing video")
+#         return
+#         # return Response("Error capturing video", status=500)
+
+#     # Perform any additional processing on the frame (e.g., add timestamp)
+#     # Modify the frame variable here to add the current time or any other desired information
+#     # if process_frame:
+#     print(plateDetect.process_image(frame))
+
+#     # Convert the frame to JPEG format
+#     # ret, jpeg = cv2.imencode('.jpg', frame)
 
 
-# @scheduler.task('interval', id='send_sms', seconds=180, misfire_grace_time=900)
 # def send_sms():
 #     # Code to send SMS goes here
 #     print("Sending SMS...")
@@ -39,17 +67,13 @@ capture = cv2.VideoCapture('./test_videos/vid3.mp4')
 #     SendSMS().sending()
 
 
-# @scheduler.scheduled_job('cron', id='send_weekly_sms', week='*', day_of_week='sun')
-# def send_weekly_reminder():
-#     print('This a weekly reminder to get rid of your trash...')
-
-# start the main scheduler
-scheduler.start()
-
-
 @app.route("/")
 def index():
-    return {
-        "message": "Index route",
-        "success": True,
-    }
+    return "Server is running"
+
+
+if __name__ == '__main__':
+    scheduler.add_job(detect_and_recognize_plate, 'interval', seconds=20)
+    # start the main scheduler
+    scheduler.start()
+    app.run(debug=True)
