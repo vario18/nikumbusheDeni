152fb70057b1d450c15eecbfcb4181e682288c86
Elia <mkumboelia@gmail.com>
2023-06-19 04:20:01 +0300

    :wrench:  improved send sms class

--- sms.py
+++ sms.py
@@ -1,30 +1,28 @@
-import africastalking
 import os
 import re
-from datetime import datetime
+import africastalking
+from dotenv import load_dotenv
 
-
-# Initialize Africa's Talking
-username = os.environ.get('AFRICAS_TALKING_USERNAME')
-api_key = os.environ.get('AFRICAS_TALKING_API_KEY')
-
-africastalking.initialize(username, api_key)
-
-
-def print_env():
-    print(f'username: {username}')
-    print(f'api_key: {api_key}')
+load_dotenv()
 
 
 class SendSMS():
-    sms = africastalking.SMS
 
-    def sending(self):
+    def __init__(self) -> None:
+        self.username = os.getenv('AFRICAS_TALKING_USERNAME')
+        self.api_key = os.getenv('AFRICAS_TALKING_API_KEY')
+
+        # Initialize the SDK
+        africastalking.initialize(self.username, self.api_key)
+
+        self.sms = africastalking.SMS
+
+    def send(self, number: str, amount: int):
         # Set the numbers in international format
-        recipients = ["+255624237298"]
+        recipients = [number]
 
         # Set your message
-        message = "Gari lako linadaiwa mzee \n #NikumbusheDeni"
+        message = f"Gari lako linadaiwa Tshs {amount}/= mzee \n #NikumbusheDeni"
 
         # Set your shortCode or senderId
         # sender = "AFRICASTKNG"
@@ -32,29 +30,6 @@ class SendSMS():
         try:
             response = self.sms.send(message, recipients)
             print(response)
-            # cache_responses[datetime.now().strftime(
-            #     "%d/%m/%Y")] = sent_number(response["Message"])
-            # sample response
-            # {
-            #     SMSMessageData: {
-            #     Message: 'Sent to 1/1 Total Cost: TZS 22.0000',
-            #     Recipients: [ [Object] ]
-            #     }
-            # }
+
         except Exception as e:
             print(f'we have a problem: {e}')
-
-
-def sent_number(message: str):
-    # input_string = "Sent to 1/1 Total Cost: TZS 22.0000"
-
-    match = re.search(r"Sent to (\d+/\d+)", message)
-
-    if match:
-        matched = match.group(1).split("/")
-        return {
-            "sent": matched[1],
-            "received": matched[0]
-        }
-    else:
-        print("None")
