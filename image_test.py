4a39eb442065fa3378cb1482e79d40d3d8e196b8
Elia <mkumboelia@gmail.com>
2023-06-19 04:27:13 +0300

    :hammer:  improved image test script to match send sms

--- image_test.py
+++ image_test.py
@@ -1,5 +1,7 @@
 import cv2
-import time
+
+from sms import SendSMS
+from src.utils import compare
 from preprocessor import PlateDetector
 
 plateDetect = PlateDetector(0.25)
@@ -7,7 +9,17 @@ plateDetect = PlateDetector(0.25)
 # cap = cv2.VideoCapture('./test_videos/vid3.mp4')
 cap = cv2.imread("./test_videos/test_pic3.jpg")
 
-plateDetect.process_image(cap)
+thePlate = plateDetect.process_image(cap)
+if thePlate is not None:
+    theCar = compare(thePlate)
+    if theCar is not None:
+        if int(theCar.owed) > 5000:
+            SendSMS().send(theCar.phoneNumber, theCar.owed)
+        else:
+            print(
+                f"{theCar.carOwner}, owner of car ({theCar.plateNumber} {theCar.plateLetter}) is not owed.")
+    else:
+        print(f"{thePlate} couldn't be found in the Database.")
 
 cap = cv2.resize(cap, (700, 700))
 cv2.imshow('frame', cap)
